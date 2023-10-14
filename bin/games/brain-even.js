01d5374f288731274f9fafde9033c9c0676239cd
Berezovsky Alexey <lazfnol@gmail.com>
2023-10-14 13:53:14 +0300

    refactor brain-even

--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -5,10 +5,10 @@ const uniqeGameText = 'Answer "yes" if the number is even, otherwise answer "no"
 
 const getRandomNumber = () => Math.floor(Math.random() * (100 - 1)) + 1;
 
-const getQuestion = () => getRandomNumber();
+const getQuestion = () => [getRandomNumber()];
 
 const getQuestionAnswer = (questionNumber) => {
-  if (questionNumber % 2 === 0) {
+  if (questionNumber[0] % 2 === 0) {
     return 'yes';
   }
   return 'no';
